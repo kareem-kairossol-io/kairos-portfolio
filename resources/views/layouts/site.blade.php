3c13e44dc05429b95e0cef99d5ae744dbfe47c85
kareem-kairossol-io <kairossys.io@gmail.com>
2026-06-26 17:46:06 +0300

    edit

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('assets/css/output.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/output.css') }}?v=1.0.1" rel="stylesheet">
     <link rel="shortcut icon" href="{{ asset('assets/imgs/favicon.ico') }}" type="image/x-icon">
     <style>
         :root {
